b6a295f92112fcbdd7609076db8f160eb86e349b
binyamin_Shapira <binyaminshp1@gmail.com>
2023-03-30 15:35:44 +0300

    Point Test

--- uintTests/primitives/PointTest.java
+++ uintTests/primitives/PointTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//Uint test for Points
 class PointTest {
 
     @Test
@@ -13,25 +14,62 @@ class PointTest {
 
         // ============ Equivalence Partitions Tests ==============
 
-
+        //01: Check sub to point, regular point
         assertEquals(new Vector(-1,-1,-1), p1.subtract(p2), "Subtract Action not correct");
 
 
         // =============== Boundary Values Tests ==================
 
-        assertThrows(IllegalArgumentException.class,()-> p1.subtract(p1),"Zero vector is created");
+        //10: Check sub same point, need throw exception
+        assertThrows(
+                IllegalArgumentException.class,
+                ()-> p1.subtract(p1),
+                "Zero vector is created");
 
     }
 
     @Test
     void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(1,2,3);
+
+        //01: Check add Vector to point
+        assertEquals(
+                new Point(2,3,4),
+                p1.add(new Vector(1,1,1)),
+                "Add action not working");
+
     }
 
     @Test
     void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(1,2,3);
+
+
+        assertEquals(
+                14,
+                p1.distanceSquared(new Point(0,0,0)),
+                0.0001,
+                "distance Squered not correct");
+
+
     }
 
     @Test
     void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(1,2,3);
+
+        //check distance between tow points
+        assertEquals(
+                Math.sqrt(14),
+                p1.distance(new Point(0,0,0)),
+                0.0001,
+                "distance not correct");
+
+
+
+
     }
 }
