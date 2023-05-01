75c0b19f026a8674b5bccbbdd2bb82ed966ef26a
ozasban <oz6962@gmail.com>
2023-05-01 23:08:00 +0300

    implement geometries tube and stert cylinder

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -4,6 +4,9 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * class that represent the Cylinder geometry
  */
@@ -51,4 +54,55 @@ public class Cylinder extends Tube{
         else
             throw new IllegalArgumentException("the point not in the range");
     }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        // Get the intersections with the tube
+        List<Point> intersections = super.findIntersections(ray);
+
+        if (intersections == null) {
+            // No intersections with the tube, so no intersections with the cylinder
+            return null;
+        }
+
+        // Calculate the intersection points with the ends of the cylinder
+        Point p0 = getAxisRay().getP0();
+        Vector v = getAxisRay().getV0().normalize();
+        Point center1 = p0.add(v.scale(height/2.0));
+        Point center2 = p0.subtract(v.scale(height/2.0));
+        Point center = new Point((center1.getX()+center2.getX())*0.5,(center1.getY()+center2.getY())*0.5,(center1.getZ()+center2.getZ())*0.5);
+
+
+        List<Point> endIntersections = new ArrayList<>();
+        double t1 = (height / 2.0 - p0.subtract(ray.getP0()).dotProduct(v)) / ray.getV0().dotProduct(v);
+        if (t1 > 0) {
+            Point intersection1 = ray.getP0().add(ray.getV0().scale(t1));
+            if (intersection1.subtract(center).lengthSquared() <= radius * radius) {
+                endIntersections.add(intersection1);
+            }
+        }
+
+        double t2 = (-height / 2.0 - p0.subtract(ray.getP0()).dotProduct(v)) / ray.getV0().dotProduct(v);
+        if (t2 > 0) {
+            Point intersection2 = ray.getP0().add(ray.getV0().scale(t2));
+            if (intersection2.subtract(center).lengthSquared() <= radius * radius) {
+                endIntersections.add(intersection2);
+            }
+        }
+
+        if (endIntersections.isEmpty()) {
+            // No intersections with the ends of the cylinder
+            return intersections;
+        }
+
+        if (intersections.isEmpty()) {
+            // No intersections with the tube, only with the ends of the cylinder
+            return endIntersections;
+        }
+
+        // Merge the intersection points from the tube and the ends of the cylinder
+        intersections.addAll(endIntersections);
+        return intersections;
+    }
 }
+
--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -4,6 +4,8 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 import static primitives.Util.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +16,7 @@ public class Tube extends RadialGeometry {
 
     /**
      * ctor
+     *
      * @param radius
      * @param axisRay
      */
@@ -28,77 +31,112 @@ public class Tube extends RadialGeometry {
 
     /**
      * get normal from tube
+     *
      * @param p point on the geometry
      * @return normalize vector
      */
     @Override
     public Vector getNormal(Point p) {
         //find the distance between p0 to o
-        double t=this.axisRay.getV0().dotProduct(p.subtract(axisRay.getP0()));
+        double t = this.axisRay.getV0().dotProduct(p.subtract(axisRay.getP0()));
         //if  p0 equivalent to p
-        if (t==0)
+        if (t == 0)
             return p.subtract(axisRay.getP0()).normalize();
         //find o
-        Point o=axisRay.getP0().add(getAxisRay().getV0().scale(t));
+        Point o = axisRay.getP0().add(getAxisRay().getV0().scale(t));
         return p.subtract(o).normalize();
     }
 
-
-
+    /**
+     * find intersections between ray and tube
+     * @param ray
+     * @return
+     */
     public List<Point> findIntersections(Ray ray) {
+        // Get the starting point of the current object's axis ray and the input Ray object
         Vector v = axisRay.getV0();
         Vector u = ray.getV0();
 
+        // Get the starting point of the current object's axis ray and the input Ray object
         Point p0 = axisRay.getP0();
         Point p1 = ray.getP0();
 
-        double a = v.dotProduct(v);
-        double b = 2 * v.dotProduct(u);
-        double c = u.dotProduct(u) - radius * radius;
+        // Calculate the necessary values for solving the quadratic equation for intersection points
+        Vector deltaP = p1.subtract(p0);
+        double deltaPDotV = deltaP.dotProduct(v);
+        double deltaPDotU = deltaP.dotProduct(u);
+        double vDotU = v.dotProduct(u);
+        double vDotV = v.dotProduct(v);
 
+        double a = vDotV * (u.lengthSquared()) - vDotU * vDotU;
+        double b = 2.0 * (vDotV * deltaPDotU - vDotU * deltaPDotV);
+        double c = vDotV * (deltaP.lengthSquared()) - (deltaPDotV * deltaPDotV) - radius * radius * vDotV;
+
+        // Calculate the discriminant of the quadratic equation
         double discriminant = b * b - 4 * a * c;
 
-        if (isZero(discriminant)) {
-            double t = -b / (2 * a);
-            Point intersectionPoint;
-            if (isZero(t))
-                intersectionPoint = ray.getP0();
-            else
-                intersectionPoint = ray.getP0().add(ray.getV0().scale(t));
+        // Check for no intersection
+        if (discriminant < 0) {
+            return null;
+        } else if (isZero(discriminant)) { // Check for a single intersection
+            // Check for no intersection when the quadratic is degenerate
+            if(isZero(a))
+                return null;
+            double t = -b / (2.0 * a);
 
+            // Check for intersection in the correct direction of the input Ray object
+            if (t <= 0) {
+                return null;
+            }
+
+            // Calculate the intersection point
+            Point intersectionPoint = p1.add(u.scale(t));
+
+            // Check if the intersection point is on the surface of the current object
             if (!isZero(intersectionPoint.distance(p0))) {
+                double deltaPDotV1 = intersectionPoint.subtract(p0).dotProduct(v);
+
+                // Check if the intersection point is inside the current object
+                if (deltaPDotU <= 0 && deltaP.subtract(v.scale(deltaP.dotProduct(v) / vDotV)).lengthSquared() > radius * radius) {
+                    return null;
+                }
+
+                // Check if the intersection point is outside the current object
+                if (deltaP.subtract(v.scale(deltaPDotV1 / vDotV)).lengthSquared() > radius * radius) {
+                    return null;
+                }
                 return List.of(intersectionPoint);
-            } else {
-                // ray is tangent to the tube, return both endpoints of the segment
-                double t1 = (p0.subtract(ray.getP0())).dotProduct(v) / v.dotProduct(v);
-                double t2 = t1 + Math.sqrt(radius * radius - p0.subtract(ray.getP0()).lengthSquared() / v.lengthSquared());
-                Point intersectionPoint1 = ray.getP0().add(ray.getV0().scale(t1));
-                Point intersectionPoint2 = ray.getP0().add(ray.getV0().scale(t2));
+            } else { // Calculate two intersection points
+                double t1 = deltaPDotV / vDotV;
+                double t2 = t1 + Math.sqrt(radius * radius / vDotV - deltaP.lengthSquared() / vDotV / vDotV);
+
+                Point intersectionPoint1 = p1.add(u.scale(t1));
+                Point intersectionPoint2 = p1.add(u.scale(t2));
 
                 return List.of(intersectionPoint1, intersectionPoint2);
             }
-        } else if (discriminant > 0) {
-            double t1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double t2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        } else { // Calculate two intersection points
+            double t1 = (-b - Math.sqrt(discriminant)) / (2.0 * a);
+            double t2 = (-b + Math.sqrt(discriminant)) / (2.0 * a);
 
-            Point intersectionPoint1 = ray.getP0().add(ray.getV0().scale(t1));
-            Point intersectionPoint2 = ray.getP0().add(ray.getV0().scale(t2));
-
-            boolean inside1 = !isZero(intersectionPoint1.distance(p0));
-            boolean inside2 = !isZero(intersectionPoint2.distance(p0));
-
-            if (inside1 && inside2) {
-                return List.of(intersectionPoint1, intersectionPoint2);
-            } else if (inside1) {
-                return List.of(intersectionPoint1);
-            } else if (inside2) {
-                return List.of(intersectionPoint2);
+            // Check for intersection in the correct direction of the input Ray object
+            List<Point> intersections = new ArrayList<>();
+            if (!isZero(t1)) {
+                Point intersectionPoint1 = p1.add(u.scale(t1));
+                if (!isZero(intersectionPoint1.distance(p0)) && intersectionPoint1.subtract(p1).dotProduct(u) > 0) {
+                    intersections.add(intersectionPoint1);
+                }
             }
+            if (!isZero(t2)) {
+                Point intersectionPoint2 = p1.add(u.scale(t2));
+                if (!isZero(intersectionPoint2.distance(p0)) && intersectionPoint2.subtract(p1).dotProduct(u) > 0) {
+                    intersections.add(intersectionPoint2);
+                }
+            }
+
+            return intersections.isEmpty() ? null : List.of(intersections.toArray(new Point[0]));
         }
-
-        return null;
     }
-
 
 
 }
